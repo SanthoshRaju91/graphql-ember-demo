a9e318939bb58d13be57796317aae7c980889159
santhoshRaju91 <santhosh.codeholic@gmail.com>
2019-03-21 22:40:01 +0530

    client:code

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
+const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 5000;
 const schema = require('./schema');
@@ -8,6 +9,7 @@ const DBConnect = require('./db');
 DBConnect()
     .then(dbMethods => {
         console.log(`Connected to mongo db`);
+        app.use(cors());
         app.use('/graphql', graphqlHTTP({
             schema,
             graphiql: true,
